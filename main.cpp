6d08cc1beacb963c4f2ca0856a4a3285c58be4b8
samurai <kbwalid26@gmail.com>
2020-07-06 18:16:39 +0900

    added sexpr

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "mpc.h"
+#include <vector>
 using namespace std;
 
 extern "C" {
@@ -7,40 +8,44 @@ char* rl_gets(const char* prompt);
 }
 
 enum LvalType {
-    LVAL_NUM, LVAL_ERR
-};
-
-enum LerrType {
-    LERR_DIV_ZERO,
-    LERR_BAD_OP,
-    LERR_BAD_NUM
+    LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_SEXPR
 };
 
 struct lval {
     LvalType type;
-    double num;
-    LerrType err;
-    lval(double x) {
-        type = LVAL_NUM;
-        num = x;
+    union {
+        double num;
+        string* err;
+        string* sym;
+        vector<lval>* cell;
+    } as;
+    lval(LvalType type, double x) : type(type) {
+        as.num = x;
     }
-    lval(LerrType x) {
-        type = LVAL_ERR;
-        err = x;
+    lval(LvalType type, const string& m) : type(type) {
+        switch (type) {
+        case LVAL_ERR:
+            as.err = new string(m); break;
+        case LVAL_SYM:
+            as.sym = new string(m); break;
+        }
     }
+    lval(LvalType type) : type(type) {
+        as.cell = new vector<lval>();
+    }
+    ~lval() {}
     ostream& print(ostream& out) const {
         switch (type) {
-        case LVAL_NUM: out << num; break;
-        case LVAL_ERR:
-            out << "Error: ";
-            switch (err) {
-            case LERR_DIV_ZERO:
-                out << "Division By Zero!"; break;
-            case LERR_BAD_OP:
-                out << "Invalid Operator!"; break;
-            case LERR_BAD_NUM:
-                out << "Invalid Number!"; break;
+        case LVAL_NUM: out << as.num; break;
+        case LVAL_ERR: out << "Error: " << *as.err; break;
+        case LVAL_SYM: out << *as.sym; break;
+        case LVAL_SEXPR:
+            out << "(";
+            for (int i = 0; i < as.cell->size(); i++) {
+                out << (*as.cell)[i];
+                if (i < as.cell->size() - 1) out << " ";
             }
+            out << ")";
         }
         return out;
     }
@@ -54,29 +59,56 @@ struct lval {
     }
 };
 
+#define NUM_VAL(x) lval(LVAL_NUM, x)
+#define SYM_VAL(x) lval(LVAL_SYM, x)
+#define ERR_VAL(x) lval(LVAL_ERR, x)
+#define SXP_VAL()  lval(LVAL_SEXPR)
+
+lval lval_read_num(mpc_ast_t* t) {
+    errno = 0;
+    double x = strtod(t->contents, NULL);
+    return errno != ERANGE ?
+        NUM_VAL(x) : ERR_VAL("invalid number");
+}
+
+lval lval_read(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) return lval_read_num(t);
+    if (strstr(t->tag, "symbol")) return SYM_VAL(t->contents);
+
+    lval x = SXP_VAL();
+    for (int i = 0; i < t->children_num; i++) {
+        if (string(t->children[i]->contents) == "(") continue;
+        if (string(t->children[i]->contents) == ")") continue;
+        if (string(t->children[i]->tag) == "regex") continue;
+        x.as.cell->push_back(lval_read(t->children[i]));
+    }
+
+    return x;
+}
+
 lval eval_op(char* op_, lval x_, lval y_) {
     if (x_.type == LVAL_ERR) return x_;
     if (y_.type == LVAL_ERR) return y_;
-    double x = x_.num;
-    double y = y_.num;
+    double x = x_.as.num;
+    double y = y_.as.num;
     string op(op_);
-    if (op == "+") { return x + y; }
-    if (op == "-") { return x - y; }
-    if (op == "*") { return x * y; }
+    if (op == "+") { return NUM_VAL(x + y); }
+    if (op == "-") { return NUM_VAL(x - y); }
+    if (op == "*") { return NUM_VAL(x * y); }
     if (op == "/") {
         return y == 0
-            ? lval(LERR_DIV_ZERO)
-            : x / y;
+            ? ERR_VAL(0)
+            : NUM_VAL(x / y);
     }
     // unreachable
-    return 0;
+    return ERR_VAL(0);
 }
 
 lval eval(mpc_ast_t* t) {
     if (strstr(t->tag, "number")) {
         errno = 0;
         double x = strtod(t->contents, NULL);
-        return errno != ERANGE ? lval(x) : lval(LERR_BAD_NUM);
+        return errno != ERANGE ? NUM_VAL(x) : ERR_VAL(0);
     }
 
     char* op = t->children[1]->contents;
@@ -93,18 +125,20 @@ int main(int argc, char *argv[]) {
     cout << "Lispy Version 0.0.0.0.1\n";
     cout << "Press Ctrl+d to Exit\n\n";
 
-    mpc_parser_t* Number   = mpc_new("number");
-    mpc_parser_t* Operator = mpc_new("operator");
-    mpc_parser_t* Expr     = mpc_new("expr");
-    mpc_parser_t* Lispy    = mpc_new("lispy");
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr  = mpc_new("sexpr");
+    mpc_parser_t* Expr   = mpc_new("expr");
+    mpc_parser_t* Lispy  = mpc_new("lispy");
 
     mpca_lang(
         MPCA_LANG_DEFAULT,
-        "number   : /[+-]?[0-9]+([.][0-9]+)?/ ;"
-        "operator : '+' | '-' | '*' | '/' ;"
-        "expr     : <number> | '(' <operator> <expr>+ ')' ;"
-        "lispy    : /^/ <expr> /$/ ;",
-        Number, Operator, Expr, Lispy);
+        "number : /[+-]?[0-9]+([.][0-9]+)?/ ;"
+        "symbol : '+' | '-' | '*' | '/' ;"
+        "sexpr  : '(' <expr>* ')' ;"
+        "expr   : <number> | <symbol> | <sexpr> ;"
+        "lispy  : /^/ <expr> /$/ ;",
+        Number, Symbol, Sexpr, Expr, Lispy);
 
     while (true) {
         char* input = rl_gets("lispy> ");
@@ -116,8 +150,10 @@ int main(int argc, char *argv[]) {
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             mpc_ast_t* output = static_cast<mpc_ast_t*>(r.output);
             mpc_ast_print(output);
-            lval result = eval(output->children[1]);
-            cout << result << "\n";
+            //lval result = eval(output->children[1]);
+            //cout << result << "\n";
+            lval v = lval_read(output->children[1]);
+            cout << v << "\n";
             mpc_ast_delete(output);
         } else {
             mpc_err_print(r.error);
@@ -125,5 +161,5 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    mpc_cleanup(4, Number, Operator, Expr, Lispy);
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 }
